2d3b75dda11b7184dbfb02a2f1e1318328b80cbd
Vaob <107950131+Vaob@users.noreply.github.com>
2020-11-21 20:48:19 +0000

    WebFoundation:

--- app/data/data.go
+++ app/data/data.go
@@ -2,4 +2,5 @@ package data // this package define and descibes behaviour of main data types in
 
 import (
 	"encoding/csv"  // all data stored in csv cache ("cache/file.csv")
-	"encoding/json" // need for parsing cand
+	"encoding/json" // need for parsing candles and responses
+	"f
