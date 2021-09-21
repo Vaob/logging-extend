073b358dec45fa2341a5eee735d12d5c0cb86014
Vaob <107950131+Vaob@users.noreply.github.com>
2021-09-21 18:17:50 +0000

    fix linux build

--- app/data/data.go
+++ app/data/data.go
@@ -148,4 +148,5 @@ func (t *Trade) MakeString() []string {
 	output[0] = fmt.Sprintf("%d", t.Id)
 	output[1] = t.Action
 	output[2] = fmt.Sprintf("%f", t.Quantity)
-	output[3] = fmt.Sprintf("%f",
+	output[3] = fmt.Sprintf("%f", t.OpenPrice)
+	o
