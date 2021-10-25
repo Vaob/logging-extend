9160f6ade6dcf0e8e81b0ccdc4a8ddbfc400c032
Vaob <107950131+Vaob@users.noreply.github.com>
2021-10-25 20:38:31 +0000

    Updating deploy script

--- app/data/data.go
+++ app/data/data.go
@@ -162,4 +162,4 @@ func (c *Candle) MakeString() []string {
 	output[2] = fmt.Sprintf("%f", c.Close)
 	output[3] = fmt.Sprintf("%f", c.High)
 	output[4] = fmt.Sprintf("%f", c.Low)
-	
+	output[5] = fmt.Sprintf("%f", 
