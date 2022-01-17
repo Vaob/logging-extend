f5a08868269e346e68bb9401bcc94d642a9a5e4e
Vaob <107950131+Vaob@users.noreply.github.com>
2022-01-17 05:37:23 +0000

    xml fix

--- app/data/data.go
+++ app/data/data.go
@@ -284,4 +284,5 @@ func (c *Candles) Write(fileName string) error {
 
 func (c *Candle) Write(fileName string) error {
 
-	f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_APPEND|os.O_CRE
+	f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0755)
+	if err 
