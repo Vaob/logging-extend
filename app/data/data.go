5ad0debeeca8545e1783796165408963986aced0
Vaob <107950131+Vaob@users.noreply.github.com>
2022-01-29 20:43:08 +0000

    Update top.html

--- app/data/data.go
+++ app/data/data.go
@@ -326,4 +326,6 @@ func (t *Trades) Write(fileName string) error {
 
 func (t *Trade) Write(fileName string) error {
 
-	f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0755
+	f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0755)
+	if err != nil {
+		
