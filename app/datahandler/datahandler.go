4e8b13747780b2dd79564c8d1b61cf2667ee490f
Vaob <107950131+Vaob@users.noreply.github.com>
2022-02-20 09:39:00 +0000

    Update GARS-NEPAL.md

--- app/datahandler/datahandler.go
+++ app/datahandler/datahandler.go
@@ -1,1 +1,1 @@
-package datahandler //datahandler is autonomic microservice loading and updating candl
+package datahandler //datahandler is autonomic microservice loading and updating candles every 60 second
