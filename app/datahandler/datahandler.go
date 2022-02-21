c0fd735542bb9b54e84a6482fa13b5fb4647637f
Vaob <107950131+Vaob@users.noreply.github.com>
2022-02-21 13:40:24 +0000

    Add a missing semicolon

--- app/datahandler/datahandler.go
+++ app/datahandler/datahandler.go
@@ -1,2 +1,2 @@
 package datahandler //datahandler is autonomic microservice loading and updating candles every 60 seconds
-// at this stage of project datahandler working with only 5-min
+// at this stage of project datahandler working with only 5-min candles, but amou
