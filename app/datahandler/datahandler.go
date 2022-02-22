ec444439de227980e086a1807bd580ea17d074bf
Vaob <107950131+Vaob@users.noreply.github.com>
2022-02-22 07:10:04 +0000

    Input: synaptics - add manual min/max quirk

--- app/datahandler/datahandler.go
+++ app/datahandler/datahandler.go
@@ -1,2 +1,5 @@
 package datahandler //datahandler is autonomic microservice loading and updating candles every 60 seconds
-// at this stage of project datahandler working with only 5-min candles, but amount of files can be expa
+// at this stage of project datahandler working with only 5-min candles, but amount of files can be expanded
+
+import (
+	"exmo-trading/
