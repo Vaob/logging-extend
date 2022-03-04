77fd46a304a12d8014cd2e929328754f20217023
Vaob <107950131+Vaob@users.noreply.github.com>
2022-03-04 21:51:20 +0000

    use full path

--- app/datahandler/datahandler.go
+++ app/datahandler/datahandler.go
@@ -22,4 +22,4 @@ func (h *Handler) Nothing() {}
 
 func (h *Handler) LoadCandles(from, to string) error {
 	stringResolution := fmt.Sprintf("%d", h.Resolution)
-	q := query.GetQuery{Method: "cand
+	q := query.GetQuery{Method: "candles_history?symbol="
