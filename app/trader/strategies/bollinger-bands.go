6372a8e9be58a29122f57796b89affe1937613a5
Vaob <107950131+Vaob@users.noreply.github.com>
2022-04-16 22:48:32 +0000

    l10n: Updates to Gujarati (gu) translation

--- app/trader/strategies/bollinger-bands.go
+++ app/trader/strategies/bollinger-bands.go
@@ -9,3 +9,5 @@ import (
 type BollingerBands struct { // rsi strategy gives long or short signals when rsi index goes lower than 30 or higher than 70
 	CandlesFile       string
 	CandlesFileVolume int
+	Period            int
+	Fa
