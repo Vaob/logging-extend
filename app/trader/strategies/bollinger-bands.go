f1d1d4afd0bc9f92cb30c8c08258278fcab63881
Vaob <107950131+Vaob@users.noreply.github.com>
2022-04-16 09:53:43 +0000

    dsfdsf

--- app/trader/strategies/bollinger-bands.go
+++ app/trader/strategies/bollinger-bands.go
@@ -6,4 +6,4 @@ import (
 	"exmo-trading/app/trader/traderutils"
 )
 
-type BollingerBands struct { // rsi strategy gives long or short signals when rsi index goes lowe
+type BollingerBands struct { // rsi strategy gives long or short signals when rsi index goes lower than 30 or higher 
