1d0227cff4c45c30e4a731939c01aa025a432b35
Vaob <107950131+Vaob@users.noreply.github.com>
2022-05-05 15:51:18 +0000

    add spec

--- app/trader/strategies/bollinger-bands.go
+++ app/trader/strategies/bollinger-bands.go
@@ -41,4 +41,4 @@ func (bb *BollingerBands) Analyze() (string, error) {
 		return signals.NoSignals, err
 	}
 	priceArray := traderutils.GetArrayFromCandles(candles)
-	ma := traderutils.GetM
+	ma := traderutils.GetMA(priceArray, bb.Pe
