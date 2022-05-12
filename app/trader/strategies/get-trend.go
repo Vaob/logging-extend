6c40f46fce6cfdb3730137a3c6b0fbfad184ae27
Vaob <107950131+Vaob@users.noreply.github.com>
2022-05-12 12:16:41 +0000

    Use

--- app/trader/strategies/get-trend.go
+++ app/trader/strategies/get-trend.go
@@ -8,3 +8,23 @@ import (
 )
 
 type GetTrend struct { // get trend gives bull or bear signals when actual price lower or hihger than ma200
+	CandlesFile       string
+	MAFrame           int
+	CandlesFileVolume int
+}
+
+func (t *GetTrend) Set(candlesFile string, candlesFileVolume int) {
+	t.CandlesFileVolume = candlesFileVolume
+	t.CandlesFile = candlesFile
+	t.MAFrame = 200
+}
+
+func (t *GetTrend) Solve(c *data.Candles, ma []float64) string {
+	length := len(ma)
+	if length != 0 {
+		if c.Array[length].Close > ma[length] {
+			return signals.Bull
+		}
+		if c.Array[length].Close < ma[length] {
+			return signals.Bear
+		}
