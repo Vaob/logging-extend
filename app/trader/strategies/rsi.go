773b4633782f51afa33718f846589545e119c842
Vaob <107950131+Vaob@users.noreply.github.com>
2022-05-12 19:48:08 +0000

    add audio

--- app/trader/strategies/rsi.go
+++ app/trader/strategies/rsi.go
@@ -5,3 +5,16 @@ import (
 	"exmo-trading/app/data"
 	"exmo-trading/app/trader/signals"
 	"exmo-trading/app/trader/traderutils"
+)
+
+type RSI struct { // rsi strategy gives long or short signals when rsi index goes lower than 30 or higher than 70
+	CandlesFile       string
+	CandlesFileVolume int
+	Period            int
+}
+
+func (rsi *RSI) Set(candlesFile string, candlesFileVolume int) {
+	rsi.Period = 14
+	rsi.CandlesFile = candlesFile
+	rsi.CandlesFileVolume = candlesFileVolume
+}
