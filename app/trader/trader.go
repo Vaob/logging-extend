78996438b967fc489b122b159be3e40e29b562ac
Vaob <107950131+Vaob@users.noreply.github.com>
2022-05-15 18:04:53 +0000

    trigger ci

--- app/trader/trader.go
+++ app/trader/trader.go
@@ -16,3 +16,27 @@ import (
 type Event struct { // event is need to making trades from strategy signals
 	//we making trade relying on previous trades and last price
 	Action    string      //handled signal:long or short
+	LastPrice float64     //actual price of traded pair
+	Trades    data.Trades //array of open trades
+}
+
+type Trader struct {
+	strategies.Strategy
+	Context TraderContext
+}
+
+type TraderContext struct {
+	TradingPair       string  `yaml:"TradingPair"`       //trading pair; exaple: "BTC_USDT"
+	TradesFile        string  `yaml:"TradesFile"`        //trades file name; example: "/cache/trades.csv"
+	TradesHistoryFile string  `yaml:"TradesHistoryFile"` //trades history file name; example: "/cache/trades-history.csv"
+	CandlesFile       string  `yaml:"CandlesFile"`       //candles file name; example: "/cache/5min-candles.csv"
+	CandlesFileVolume int     `yaml:"CandlesFileVolume"` // volume of candles in candles file; default: 250
+	TradesFileVolume  int     `yaml:"TradesFileVolume"`  // max amount of open trades; default: 10 (means 10 trades)
+	StopLimitPercent  float64 `yaml:"StopLimitPercent"`  // 0.01 means 1%, when price change goes higher
+	// or lower this percent, we close unprofitable trade
+	// default: 0.01
+	TradeAmount   float64       `yaml:"TradeAmount"`   // trade volume; example: 100 USDT in BTC_USDT pair
+	TraderTimeout time.Duration `yaml:"TraderTimeout"` // time of trader reload; default: 30
+}
+
+func (t *TraderContext) Nothing() {
