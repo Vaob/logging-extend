46cdea98e9341534822b2a7d66faa9c7a679bed8
Vaob <107950131+Vaob@users.noreply.github.com>
2022-05-19 13:57:59 +0000

    0.1.10

--- app/trader/traderutils/traderutils.go
+++ app/trader/traderutils/traderutils.go
@@ -14,3 +14,20 @@ func GetMA(array []float64, frame int) []float64 {
 		sum := 0.00
 		for j := i - frame; j < i; j++ {
 			sum = sum + array[j]
+		}
+		ma[i] = sum / float64(frame)
+	}
+	return ma
+}
+
+func GetSD(priceArray, ma []float64, frame int) []float64 {
+	length := len(priceArray)
+	sd := make([]float64, length)
+	for i := frame; i < length; i++ {
+		sum := 0.00
+		for j := i - frame; j < i; j++ {
+			sum = sum + (priceArray[j]-ma[i])*(priceArray[j]-ma[i])
+		}
+		sd[i] = math.Sqrt(sum / float64(frame))
+	}
+	return sd
