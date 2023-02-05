b21a06c926ea0a1f7cf9155ba31ce0bdd2729d31
Vaob <107950131+Vaob@users.noreply.github.com>
2023-02-05 16:32:09 +0000

    rename module

--- exmo-trader.go
+++ exmo-trader.go
@@ -50,4 +50,4 @@ func PrepareApp() *App {
 	time.Sleep(1 * time.Second)
 	app.Traders = append(app.Traders, PrepareTrader(path, path+
 		"/configs/trader-configs/5min-btc-usdt-trader.yaml", &strategies.BollingerBands{}))
-	app.Traders = append(app.Tra
+	app.Traders = append(app.Traders, PrepareTrade
